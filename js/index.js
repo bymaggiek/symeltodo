0ea56bb09c49197c7babfd7ee13ace971b71b605
Yiteng Huang <hyy528924543@gmail.com>
2021-05-31 15:05:15 +1000

    finish add card function, delete and reset remaining

--- js/index.js
+++ js/index.js
@@ -1,33 +1,65 @@
+const startCol = document.getElementById("startCol");
+const inProgressCol = document.getElementById("inProgressCol");
+const inReviewCol = document.getElementById("inReviewCol");
 const completeBtn = document.getElementById("completeBtn");
 const completeCol = document.getElementById("completeCol");
 const angle = document.getElementById("angle");
 
-let taskName = document.getElementById("taskName");
+const saveBtn = document.getElementById("saveBtn");
+const resetBtn = document.getElementById("resetBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+const addTaskBtn = document.getElementById("addCardForm");
+
+const taskName = document.getElementById("taskName");
 const description = document.getElementById("description");
-let dueDate = document.getElementById("dueDate");
-let assignedPPL = document.getElementById("assignedPPL");
+const dueDate = document.getElementById("dueDate");
+const assignedPPL = document.getElementById("assignedPPL");
 const status = document.getElementById("status");
 
-const startCol = document.getElementById("startCol");
-const inProgressCol = document.getElementById("inProgressCol");
-const inReviewCol = document.getElementById("inReviewCol");
-
-const card = document.createElement("div"); // .card
-const cardHeader = document.createElement("div"); //.card-header
-const cardHeaderP = document.createElement("p"); // .card-header > p
-const cardHeaderI = document.createElement("i"); // .card-header > i
-const cardDescription = document.createElement("div"); // .card-description
-const cardDescriptionP = document.createElement("p"); // .card-description > p
-const cardFooter = document.createElement("div"); //.card-footer
-const cardAssigned = document.createElement("p"); // .card-footer > p
-const cardDueDate = document.createElement("p"); // .card-footer > p
-
-const saveBtn = document.getElementById("saveBtn");
-
+const startArr = JSON.parse(localStorage.getItem("startArr"))
+  ? JSON.parse(localStorage.getItem("startArr"))
+  : [];
+const inProgressArr = JSON.parse(localStorage.getItem("inProgressArr"))
+  ? JSON.parse(localStorage.getItem("inProgressArr"))
+  : [];
+const inReviewArr = JSON.parse(localStorage.getItem("inReviewArr"))
+  ? JSON.parse(localStorage.getItem("inReviewArr"))
+  : [];
+const completedArr = JSON.parse(localStorage.getItem("completeArr"))
+  ? JSON.parse(localStorage.getItem("completeArr"))
+  : [];
+if (startArr) {
+  startArr.forEach((arr) => {
+    addCard(arr, "to-start");
+  });
+}
+if (inProgressArr) {
+  inProgressArr.forEach((arr) => {
+    addCard(arr, "in-progress");
+  });
+}
+if (inReviewArr) {
+  inReviewArr.forEach((arr) => {
+    addCard(arr, "in-review");
+  });
+}
+if (completedArr) {
+  completedArr.forEach((arr) => {
+    addCard(arr, "complete");
+  });
+}
 let completeBtnCount = 0;
 
+// Show "create" button and Hide "reset" and "delete" buttons
+addTaskBtn.addEventListener("click", () => {
+  resetBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+  saveBtn.style.display = "block";
+  clearInput();
+});
+
 // Trigger Completed Section Show/Hide
-completeBtn.onclick = () => {
+completeBtn.addEventListener("click", () => {
   if (completeBtnCount === 0) {
     completeCol.style.display = "flex";
     angle.classList.replace("fa-angle-double-down", "fa-angle-double-up");
@@ -37,72 +69,70 @@ completeBtn.onclick = () => {
     angle.classList.replace("fa-angle-double-up", "fa-angle-double-down");
     completeBtnCount = 0;
   }
-};
+});
 
 // Save data as nested array under different categories - toStart, inProgress, inReview and Completed
-saveBtn.onclick = () => {
+saveBtn.addEventListener("click", () => {
   if (taskName.value && dueDate.value && assignedPPL.value) {
     switch (status.value) {
       case "to-start":
-        const startArr = JSON.parse(localStorage.getItem("startArr"))
-          ? JSON.parse(localStorage.getItem("startArr"))
-          : [];
-        const newArr = [
+        const startSub = [
           startArr.length,
           taskName.value,
           description.value,
           dueDate.value,
           assignedPPL.value,
-          status.value,
+          0,
         ];
-        startArr.push(newArr);
+        startArr.push(startSub);
         localStorage.setItem("startArr", JSON.stringify(startArr));
-        updateColumn("to-start", startArr);
+        addCard(startSub, "to-start");
+        addCard.called ? clearInput() : console.log("finish the card first");
         break;
 
       case "in-progress":
-        const inProgressArr = JSON.parse(localStorage.getItem("inProgressArr"))
-          ? JSON.parse(localStorage.getItem("inProgressArr"))
-          : [];
-        inProgressArr.push([
+        const progressSub = [
           inProgressArr.length,
           taskName.value,
           description.value,
           dueDate.value,
           assignedPPL.value,
-          status.value,
-        ]);
+          1,
+        ];
+        inProgressArr.push(progressSub);
         localStorage.setItem("inProgressArr", JSON.stringify(inProgressArr));
+        addCard(progressSub, "in-progress");
+        addCard.called ? clearInput() : console.log("finish the card first");
         break;
 
       case "in-review":
-        const inReviewArr = JSON.parse(localStorage.getItem("inReviewArr"))
-          ? JSON.parse(localStorage.getItem("inReviewArr"))
-          : [];
-        inReviewArr.push([
+        const reviewSub = [
           inReviewArr.length,
           taskName.value,
           description.value,
           dueDate.value,
           assignedPPL.value,
-          status.value,
-        ]);
+          2,
+        ];
+        inReviewArr.push(reviewSub);
         localStorage.setItem("inReviewArr", JSON.stringify(inReviewArr));
+        addCard(reviewSub, "in-review");
+        addCard.called ? clearInput() : console.log("finish the card first");
         break;
 
       case "complete":
-        const completedArr = JSON.parse(localStorage.getItem("completedArr"))
-          ? JSON.parse(localStorage.getItem("completedArr"))
-          : [];
-        completedArr.push([
+        const completeSub = [
           completedArr.length,
           taskName.value,
           description.value,
           dueDate.value,
           assignedPPL.value,
-          status.value,
-        ]);
+          3,
+        ];
+        completedArr.push(completeSub);
         localStorage.setItem("completeArr", JSON.stringify(completedArr));
+        addCard(completeSub, "complete");
+        addCard.called ? clearInput() : console.log("finish the card first");
         break;
 
       default:
@@ -113,65 +143,119 @@ saveBtn.onclick = () => {
         }
     }
   }
-};
+});
 
-// Add card to different columns based on category
-updateColumn = (status, str) => {
+// Reset data for the chosen card
+resetBtn.addEventListener("click", () => {});
 
-  str.forEach(subStr => {
+// Delete card from LS
 
-    cardHeaderP.innerHTML = `${subStr[1]}`;
-    cardHeaderP.className = "card-title";
-    cardHeaderI.className = "fas fa-pen";
-    cardHeader.className = "card-header";
-    cardHeader.appendChild(cardHeaderP);
-    cardHeader.appendChild(cardHeaderI);
-  
-    cardDescriptionP.innerHTML = `${subStr[2]}`;
-    cardDescription.className = "card-description"
-    cardDescription.appendChild(cardDescriptionP);
+// Show "reset" and "delete" buttons and Hide "create" button
+function resetBtnTrigger(index, status) {
+  resetBtn.style.display = "block";
+  deleteBtn.style.display = "block";
+  saveBtn.style.display = "none";
 
-    cardAssigned.innerHTML = `${subStr[4]}`;
-    cardAssigned.className = "card-assigned";
-    cardDueDate.innerHTML = `${subStr[3]}`;
-    cardDueDate.className = "due-date";
-    cardFooter.className = "card-footer";
-    cardFooter.appendChild(cardAssigned);
-    cardFooter.appendChild(cardDueDate);
-
-    card.className = "card";
-    card.appendChild(cardHeader);
-    card.appendChild(cardDescription);
-    card.appendChild(cardFooter);
-
-    switch (status) {
-    case "to-start":
-      startCol.className = "card-display";
-      startCol.appendChild(card);
-      break;
-
-    case "in-progress":
-      inProgressCol.className = "card-display";
-      inProgressCol.appendChild(card);
-      break;
-
-    case "in-review":
-      inReviewCol.className = "card-display";
-      inReviewCol.appendChild(card);
-      break;
-
-    case "complete":
-      completeCol.className = "complete";
-      completeCol.appendChild(card);
-      break;
-
-    default:
+  if (status === 0) {
+    document.getElementById("status").value = "to-start";
+    const triggeredArr = JSON.parse(localStorage.getItem("startArr"));
+    triggerInfo(triggeredArr, index);
+  } else if (status === 1) {
+    document.getElementById("status").value = "in-progress";
+    const triggeredArr = JSON.parse(localStorage.getItem("inProgressArr"));
+    triggerInfo(triggeredArr, index);
+  } else if (status === 2) {
+    document.getElementById("status").value = "in-review";
+    const triggeredArr = JSON.parse(localStorage.getItem("inReviewArr"));
+    triggerInfo(triggeredArr, index);
+  } else if (status === 3) {
+    document.getElementById("status").value = "complete";
+    const triggeredArr = JSON.parse(localStorage.getItem("completedArr"));
+    triggerInfo(triggeredArr, index);
+  } else {
+    try {
+      document.getElementById("status").value = "to-start";
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+// Helper function to get specific array and its elements by arr[index]
+function triggerInfo(arr, index) {
+  taskName.value = arr[index][1];
+  description.value = arr[index][2];
+  dueDate.value = arr[index][3];
+  assignedPPL.value = arr[index][4];
+  for (a in arr) {
+    if (arr[index][5] === 0) {
+      status.value = "to-start";
+    } else if (arr[index][5] === 1) {
+      status.value = "in-progress";
+    } else if (arr[index][5] === 2) {
+      status.value = "in-review";
+    } else if (arr[index][5] === 3) {
+      status.value = "complete";
+    } else {
       try {
-        break;
+        status.value = "to-start";
       } catch (e) {
         console.log(e);
       }
+    }
   }
-})
+  tempArr = [
+    taskName.value,
+    description.value,
+    dueDate.value,
+    assignedPPL.value,
+    status.value,
+  ];
+}
 
-};
+// Add card to different columns based on category
+function addCard(arr, status) {
+  const card = document.createElement("div");
+  card.className = "card";
+  addCard.called = true;
+
+  if (status === "to-start") {
+    const cardDisplay = document.getElementById("startCol");
+    cardDisplay.appendChild(card);
+  } else if (status === "in-progress") {
+    const cardDisplay = document.getElementById("inProgressCol");
+    cardDisplay.appendChild(card);
+  } else if (status === "in-review") {
+    const cardDisplay = document.getElementById("inReviewCol");
+    cardDisplay.appendChild(card);
+  } else if (status === "complete") {
+    const cardDisplay = document.getElementById("completeCol");
+    cardDisplay.appendChild(card);
+  } else {
+    console.log("invalid status type detected");
+  }
+  card.innerHTML = `
+      <div class="card-header">
+        <p class="card-title">${arr[1]}</p>
+        <i class="fas fa-pen" data-bs-toggle="modal"
+          data-bs-target="#modal" onClick=resetBtnTrigger(${arr[0]},${arr[5]})></i>
+      </div>
+      <div class="card-description">
+        <p>${arr[2]}</p>
+      </div>
+      <div class="card-footer">
+        <p class="card-assigned">${arr[4]}</p>
+        <p class="due-date">${arr[3]}</p>
+      </div>
+  `;
+}
+
+// Clear form inputs once click the "create" button
+function clearInput() {
+  document.getElementById("taskName").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("dueDate").value = "";
+  document.getElementById("assignedPPL").value = "";
+  document.getElementById("status").value = "to-start";
+}
+
+function changeCardInfo(arr) {}
